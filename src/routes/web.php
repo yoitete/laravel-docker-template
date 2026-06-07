fe62ff604aea6ca66a3a486835570b34dec1506c
yoitete <kh.70239568@gmail.com>
2026-06-07 13:31:07 +0900

    一覧画面のURIの設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/todo', function () {
+    echo 'Hello World!';
+});
 
 Route::get('/', function () {
     return view('welcome');
