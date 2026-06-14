84fcf12791aa70cfc02817c53e29e37205affff3
yoitete <kh.70239568@gmail.com>
2026-06-14 18:38:35 +0900

    docs: todosマイグレーションに説明コメントを追加

--- src/database/migrations/2026_06_05_085641_create_todos_table.php
+++ src/database/migrations/2026_06_05_085641_create_todos_table.php
@@ -11,9 +11,13 @@ class CreateTodosTable extends Migration
      *
      * @return void
      */
+   
     public function up()
     {
+        // Migration (マイグレーション) : テーブルの作成を担う
+        // マイグレーションファイルは、upメソッドに記載されているテーブル情報をもとにテーブルを作成
         Schema::create('todos', function (Blueprint $table) {
+
             $table->bigIncrements('id');
             $table->string('content');
             $table->timestamps();
@@ -25,8 +29,15 @@ class CreateTodosTable extends Migration
      *
      * @return void
      */
+    // down : upメソッドの逆の操作としてtodosテーブルを削除する実装がされている。
     public function down()
     {
+        {
         Schema::dropIfExists('todos');
+        }
     }
 }
+
+// マイグレーションを使う理由は2つ
+// SQLを知らなくても、PHPコードでテーブル操作ができるため学習コストが不要
+// 現在のデータベースの状態を他の開発者に共有することができる
