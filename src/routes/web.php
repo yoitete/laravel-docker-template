dcf875234c3ee5a737e6f8ec32877aa58c5ba356
yoitete <kh.70239568@gmail.com>
2026-06-14 18:38:09 +0900

    feat: todoの詳細・編集・更新・削除ルートの追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -10,12 +10,20 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// ギズテックsection2
 Route::get('/todo', 'TodoController@index')->name('todo.index');
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
 Route::post('/todo', 'TodoController@store')->name('todo.store');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// /{id} /todo/1や/todo/2の「1」や「2」のような変数の部分をルートパラメータ
+// idなどその情報を特定するためにしている
+Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
+Route::get('/todo/{id}/edit', 'TodoController@edit')->name('todo.edit');
+Route::put('/todo/{id}', 'TodoController@update')->name('todo.update');
+Route::delete('/todo/{id}', 'TodoController@delete')->name('todo.delete');
 
+Route::get('/study', 'studyController@index')->name('study.index');
+Route::get('/study/create', 'studyController@create')->name('study.create');
+Route::post('/study', 'studyController@store')->name('study.store');
 
+Route::get('/', function () {});
