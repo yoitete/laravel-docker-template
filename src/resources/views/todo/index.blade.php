68da3fdbec865f2e670e3a88703178ae933664c9
yoitete <kh.70239568@gmail.com>
2026-06-14 18:37:02 +0900

    feat: todoの編集・削除・詳細表示機能の追加

--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -10,9 +10,17 @@
                 ToDo一覧
               </div>
               <div class="list-group list-group-flush">
+                
+  <!-- - 左：繰り返す対象 Collection
+       - as：「〜を 〜として（取り出す）」
+       - 右：1個ずつ取り出した値を受け取る変数 
+       - $todosを$todoとして1件ずつ取り出す -
+       -->
                 @foreach ($todos as $todo)
                   <div class="d-flex align-items-center p-2">
                     <span class="col-9">{{ $todo->content }}</span>
+                  <!--http://localhost:8080/todo/1になる-->
+                    <a href="{{ route('todo.show', $todo->id) }}" class="btn btn-info ml-3">詳細</a>
                   </div>
                 @endforeach
               </div>
@@ -21,3 +29,4 @@
         </div>
      </div>
      @endsection
+     
